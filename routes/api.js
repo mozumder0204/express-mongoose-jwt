3c7e9479beebd578c9833016f9ab1fc2ba21ddd2
Billal Hossain <mozumder0204@outlook.com>
2022-02-17 01:37:53 +0600

    public API OK

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var publicRouter = require('./public');
 var authRouter = require('./auth');
 var productRouter = require('./product');
 
 var app = express();
 
+app.use('/public/', publicRouter);
 app.use('/auth/', authRouter);
 app.use('/product/', productRouter);
 
